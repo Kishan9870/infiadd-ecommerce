b8381a6097d7b3ed0c1a62c2a093b1b457a4cd02
Kishan9870 <kishan.infiadd@gmail.com>
2024-02-08 15:04:56 +0530

    refactored code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,25 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/admin/login/login.component';
-import { StyleSwitcherComponent } from './components/style-switcher/style-switcher.component';
-import { LandingComponent } from './pages/website/landing/landing.component';
 
 export const routes: Routes = [
-    // {
-    //     path: '',
-    //     redirectTo: 'login',
-    //     pathMatch: 'full'
-    // },
     {
         path: '',
-        redirectTo: 'landing-page',
+        redirectTo: 'login',
         pathMatch: 'full'
     },
     {
         path: 'login',
         component: LoginComponent
-    },
-    {
-        path: 'landing-page',
-        component: LandingComponent
     }
 ];
