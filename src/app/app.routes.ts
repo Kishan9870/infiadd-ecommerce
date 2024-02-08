b4f31c4df6bd9f7947d063f607d6e50896afceac
Kishan9870 <kishan.infiadd@gmail.com>
2024-02-08 12:32:38 +0530

    added material ui

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,25 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/admin/login/login.component';
 import { StyleSwitcherComponent } from './components/style-switcher/style-switcher.component';
+import { LandingComponent } from './pages/website/landing/landing.component';
 
 export const routes: Routes = [
+    // {
+    //     path: '',
+    //     redirectTo: 'login',
+    //     pathMatch: 'full'
+    // },
     {
         path: '',
-        redirectTo: 'login',
+        redirectTo: 'landing-page',
         pathMatch: 'full'
     },
     {
         path: 'login',
         component: LoginComponent
+    },
+    {
+        path: 'landing-page',
+        component: LandingComponent
     }
 ];
--- src/app/pages/website/landing/landing.component.ts
+++ src/app/pages/website/landing/landing.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SideNavbarComponent } from '../../../components/side-navbar/side-navbar.component';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [],
+  imports: [SideNavbarComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
